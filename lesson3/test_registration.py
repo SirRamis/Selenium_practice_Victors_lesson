59872f3938adad6576170c20f70207f11869498a
Ramis <sir.ramis@mail.ru>
2024-04-11 14:01:01 +0300

    add def test registration

--- lesson3/test_registration.py
+++ lesson3/test_registration.py
@@ -1,10 +1,19 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 from practice.src import urls
 from selenium.webdriver.support import expected_conditions as EC
 
-ELEMENT_START_TEST = ("xpath", "//button[text()='Начать тестирование']")
+START_TEST_BUTTON = ("xpath", "//button[text()='Начать тестирование']")
 HEADER_h1 = ("xpath", "/html/body/h1")
+REG_FORM = ("xpath", '//*[@id="registrationForm"]/label[1]')
+USERNAME_FIELD = ('xpath', '//*[@id="login"]')
+PASSWORD_FIELD = ('xpath', '//*[@id="password"]')
+AGREE = ('xpath', '//*[@id="agree"]')
+REG_BUTTON = ("xpath", '//*[@id="register"]')
+MESSAGE = ('xpath', '//*[@id="successMessage"]')
+
 
 def test1_actual_text(chrome, wait):
     chrome.get(urls.Urls.reg_url)
@@ -13,14 +22,27 @@ def test1_actual_text(chrome, wait):
 
 def test2_wait_elemet_start_test(chrome, wait):
     chrome.get(urls.Urls.reg_url)
-    visible_after_button = wait.until(EC.element_to_be_clickable((ELEMENT_START_TEST)))
+    visible_after_button = wait.until(EC.element_to_be_clickable((START_TEST_BUTTON)))
     assert visible_after_button.text == 'Начать тестирование'
 
-def test_registration(chrome):
+def test3_registration(chrome, wait):
     chrome.get(urls.Urls.reg_url)
-    chrome.find_element(ELEMENT_START_TEST).click()
+    wait.until(EC.element_to_be_clickable((START_TEST_BUTTON)))
+    chrome.find_element(*START_TEST_BUTTON).click()
+    time.sleep(3)
+    visible_after_button = chrome.find_element(*REG_FORM).text
+    assert visible_after_button == 'Логин:'
 
-def test_checking_the_registration_functionality_on_the_site(chrome, wait):
+def test4_registration(chrome, wait):
     chrome.get(urls.Urls.reg_url)
-    visible_after_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Начать тестирование']")))
-    assert visible_after_button.text == 'Начать тестирование'
+    wait.until(EC.element_to_be_clickable((START_TEST_BUTTON)))
+    chrome.find_element(*START_TEST_BUTTON).click()
+    time.sleep(3)
+    chrome.find_element(*USERNAME_FIELD).send_keys("login")
+    chrome.find_element(*PASSWORD_FIELD).send_keys("password")
+    chrome.find_element(*AGREE).click()
+    chrome.find_element(*REG_BUTTON).click()
+    time.sleep(4)
+    expected_text = chrome.find_element(*MESSAGE).text
+    actual_text = 'Вы успешно зарегистрированы!'
+    assert expected_text == actual_text
