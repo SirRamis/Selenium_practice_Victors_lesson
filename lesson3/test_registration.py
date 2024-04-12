210e130643044e7714a17ad3f6e93d4ed152f2fd
Ramis <sir.ramis@mail.ru>
2024-04-12 14:26:03 +0300

    add def test color

--- lesson3/test_registration.py
+++ lesson3/test_registration.py
@@ -1,5 +1,6 @@
 import time
 
+from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
 
 from practice.src import urls
@@ -46,3 +47,68 @@ def test4_registration(chrome, wait):
     expected_text = chrome.find_element(*MESSAGE).text
     actual_text = 'Вы успешно зарегистрированы!'
     assert expected_text == actual_text
+
+def test1_add(chrome):
+    chrome.get('https://the-internet.herokuapp.com/add_remove_elements/')
+    chrome.find_element('xpath','//*[@id="content"]/div/button').click()
+    a = chrome.find_element('xpath','//*[@id="elements"]/button')
+    assert a.is_displayed()
+
+def test1_add_delete(chrome):
+    chrome.get('https://the-internet.herokuapp.com/add_remove_elements/')
+    time.sleep(3)
+    chrome.find_element('xpath','//*[@id="content"]/div/button').click()
+    chrome.find_element('xpath','//*[@id="elements"]/button').click()
+    time.sleep(3)
+    assert not chrome.find_elements('xpath', '//*[@id="elements"]/button'), 'В корзине есть товары'
+
+
+def test2_auth(chrome):
+    username = "admin"
+    password = "admin"
+    url = f"https://{username}:{password}@the-internet.herokuapp.com/basic_auth"
+    chrome.get(url)
+    time.sleep(3)
+    expected_text = chrome.find_element('xpath','//*[@id="content"]/div/p').text
+    assert expected_text == 'Congratulations! You must have the proper credentials.', "Registration was not successful"
+
+    # # Ждем появления элемента на странице с помощью WebDriverWait
+    # try:
+    #     element = WebDriverWait(driver, 10).until(
+    #         EC.presence_of_element_located((By.TAG_NAME, "h3"))  # Пример: ждем появления элемента <h3>
+    #     )
+    #     print("Элемент появился на странице:", element.text)
+    # except:
+    #     print("Элемент не появился на странице")
+    # finally:
+    #     driver.quit()
+
+from PIL import Image
+
+image_path = "https://the-internet.herokuapp.com/broken_images" # Укажите путь к вашему изображению
+def test_check_image_integrity(chrome):
+    chrome.get('https://the-internet.herokuapp.com/broken_images')
+    time.sleep(3)
+    try:
+        with Image.open('https://the-internet.herokuapp.com/img/avatar-blank.jpg') as img:
+            width, height = img.size
+            if width > 0 and height > 0:
+                print("Изображение целостное.")
+            else:
+                print("Изображение повреждено или не может быть загружено.")
+    except Exception as e:
+        print("Ошибка при загрузке изображения:", e)
+
+        images = chrome.execute_script("return document.getElementsByTagName('img');")
+        for image in images:
+            src = image.get_attribute("src")
+            width = chrome.execute_script("return arguments[0].naturalWidth;", image)
+            if width == 0:
+                print(f"Broken image found: {src}")
+
+    # Navigate to url
+
+def test_color(chrome):
+    chrome.get("'https://www.selenium.dev/selenium/web/colorPage.html'")
+    cssValue = chrome.find_element(By.ID, "namedColor").value_of_css_property('background-color')
+    print(cssValue)
--- lesson3/test_test.py
+++ lesson3/test_test.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -25,3 +27,49 @@ def test_visible_after_with_explicit_waits(driv, wait):
     driv.get('https://demoqa.com/dynamic-properties')
     visible_after_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Visible After 5 Seconds']")))
     assert visible_after_button.text == 'Visible After 5 Seconds'
+
+
+
+# Инициализация драйвера браузера
+def test_qw():
+    driver = webdriver.Chrome()
+    driver.get("https://<username>:<password>@the-internet.herokuapp.com/basic_authl")
+    driver.get("https://the-internet.herokuapp.com/basic_authl")
+
+
+def test_qwt():
+    username = "your_username"
+    password = "your_password"
+
+    # Создаем экземпляр драйвера Chrome
+    driver = webdriver.Chrome()
+    time.sleep(3)
+    # Формируем URL для авторизации HTTP Basic
+    url = f"https://{username}:{password}@the-internet.herokuapp.com/basic_auth"
+    time.sleep(3)
+    # Переходим по URL
+    driver.get(url)
+
+    # Продолжайте ваш тест здесь...
+
+# Загрузка страницы
+#driver.get("https://the-internet.herokuapp.com/basic_authl")
+
+# Ожидание появления всплывающего окна
+# popup_element = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.XPATH, "your_popup_xpath"))
+# )
+#
+# # Находим поля ввода для имени пользователя и пароля
+# username_field = popup_element.find_element(By.XPATH, "your_username_input_xpath")
+# password_field = popup_element.find_element(By.XPATH, "your_password_input_xpath")
+#
+# # Вводим логин и пароль
+# username_field.send_keys("your_username")
+# password_field.send_keys("your_password")
+#
+# # Находим кнопку входа и кликаем на нее
+# login_button = popup_element.find_element(By.XPATH, "your_login_button_xpath")
+# login_button.click()
+
+
